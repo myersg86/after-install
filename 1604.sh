ad4b3309934d4ea592132a520079e819320d67cb
Greg Myers <myersg86@gmail.com>
2017-02-16 10:25:11 -0800

    Ubuntu16.04 After-Install Setup Script

---
+++ 1604.sh
@@ -0,0 +1,7 @@
+sudo apt install software-properties-common python-software-properties &&
+sudo add-apt-repository ppa:numix/ppa -y && 
+sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y &&
+sudo add-apt-repository ppa:webupd8team/atom -y &&
+sudo apt update &&
+sudo apt upgrade -y && 
+sudo apt install atom autoconf automake bash-completion build-essential curl  faac  faad  ffmpeg2theora  filezilla flac  font-manager ffmpeg gdebi geany geany-plugin* git gparted  gzip htop  icedax  id3v2  lame  liba52-dev  libdvdcss2  libdvdnav4 libdvdread4  libjpeg-progs  libmad0  libmpeg2-4  linux-tools-common  linux-tools-generic  mencoder  meld mpeg2dec  mpeg3-utils  mpegdemux  mpg123  mpg321  mpv nano  net-tools  nmap  numix-* openjdk-8-jre-headless  openssh-client  openssh-server  openssh-sftp-server p7zip-full  p7zip-rar  pinta redshift sox  sublime-text-installer synaptic tar ttf-ubuntu-font-family ubuntu-restricted-* uget  unzip  uudeview   vim  vlc vorbis-tools  wget youtube-dl zip  
