b21137eda9785578741b894488f9d697da842a38
Greg Myers <myersg86@gmail.com>
2017-09-17 17:47:28 -0700

    Update 1704.sh

--- 1704.sh
+++ 1704.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -x
 sudo apt install -y software-properties-common python-software-properties &&
-# sudo add-apt-repository -y ppa:ubuntu-mozilla-security/ppa 
-#SUBLIME TEXT
-# sudo add-apt-repository -y ppa:webupd8team/sublime-text-3  &&
+# Mozilla's latest:
+sudo add-apt-repository -y ppa:ubuntu-mozilla-security/ppa 
 #ATOM EDITOR
 # sudo add-apt-repository -y ppa:webupd8team/atom &&
 #VSCODE
@@ -28,7 +27,6 @@ sudo apt update &&
 sudo apt install atom autoconf autogen automake bash-completion build-essential checkinstall cvs curl dos2unix faac faad ffmpeg2theora filezilla flac font-manager ffmpeg gdebi geany geany-plugin* golang-go gimp git git-core gparted gzip html2text htop icedax id3v2 inkscape lame liba52-dev libdvdcss2 libdvdnav4 libdvdread4 libjpeg-progs libmad0 libmpeg2-4 libreoffice linux-tools-common linux-tools-generic lynx markdown mencoder meld mercurial mpeg2dec mpeg3-utils mpegdemux mpg123 mpg321 mpv nano net-tools nmap numix-* openjdk-8-jre-headless openssh-client openssh-server openssh-sftp-server subversion php python python-dev python-gpgme python-pip p7zip p7zip-full p7zip-rar pinta poppler-utils unrar redshift sox shellcheck synaptic tar tmux ttf-ubuntu-font-family ubuntu-make ubuntu-restricted-* uget unzip uudeview vim vim-scripts vim-syntastic vlc vorbis-tools ruby-full wget youtube-dl zip &&
 sudo -H pip install --upgrade pip
 sudo -H pip install cheat &&
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - &&
-sudo apt update -y &&
-sudo apt dist-upgrade -y &&
-sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - &&
+sudo apt-get install -y nodejs &&
+sudo apt dist-upgrade -y
